1c6d0a02f24139155d352fe5f160fb3a0cc94150
Bzulee <biplov.jung99@gmail.com>
2022-11-14 11:13:05 +0545

    Initial Commit

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+<head>
+<h1>Username: {{$username}}</h1>
+<p>This is my profile page</p>
+</head>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <link rel="" href="{{asset('css/style.css')}}">
+</head>
+<body>
+<h1>{{$name}},{{$age}}</h1>
+<p>Hello ma BZULEE</p></body>
+</html>
+
